cd966ad17e583d136fff0af6d7456ce7450c0243
Tim Lucas <t.lucas@toolmantim.com>
2009-09-16 23:27:44 +1000

    Test the Toadhopper::Test methods

--- test/test_test.rb
+++ test/test_test.rb
@@ -3,10 +3,10 @@ require File.dirname(__FILE__) + "/../lib/toadhopper/test"
 require 'test/unit'
 
 class Toadhopper::TestTest < Test::Unit::TestCase
+  include Toadhopper::Test::Methods
   def test_stub_toadhopper_post!
-    # TODO:
-  end
-  def test_last_toadhopper_post_params
-    # TODO:
+    stub_toadhopper_post!
+    Toadhopper.post!(:error, :options, :header_options)
+    assert_equal [:error, :options, :header_options], last_toadhopper_post_arguments
   end
 end
