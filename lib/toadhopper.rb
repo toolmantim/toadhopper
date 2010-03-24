ab41c9fb5eb58360b907587aaf267b66b3ce0f32
Corey Donohoe <atmos@atmos.org>
2010-03-24 11:19:29 -0700

    next version is 1.0.3

--- lib/toadhopper.rb
+++ lib/toadhopper.rb
@@ -4,7 +4,7 @@ require 'ostruct'
 
 # Posts errors to the Hoptoad API
 class Toadhopper
-  VERSION = "1.0.2"
+  VERSION = "1.0.3"
 
   # Hoptoad API response
   class Response < Struct.new(:status, :body, :errors); end
