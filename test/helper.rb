2494e8e21d124dfdde87d2067b609b26f8abca7a
Tim Lucas <t.lucas@toolmantim.com>
2010-04-01 12:48:15 +1100

    Remove all this File.expand_path non-sense, the TestTask already adds lib and test to the $LOAD_PATH

--- test/helper.rb
+++ test/helper.rb
@@ -2,8 +2,7 @@ Bundler.setup(:test)
 Bundler.require(:test)
 
 require 'test/unit'
-
-require File.expand_path("../../lib/toadhopper", __FILE__)
+require "toadhopper"
 
 def toadhopper
   @toadhopper ||= Toadhopper.new(ENV['HOPTOAD_API_KEY'] || "test api key")
--- test/test_posting.rb
+++ test/test_posting.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.join(File.dirname(__FILE__), 'helper'))
+require 'helper'
 
 class Toadhopper::TestPosting < Test::Unit::TestCase
   def test_posting
