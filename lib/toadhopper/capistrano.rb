346dffe658e7a2ae72c1eb68abfbf4f407c7d382
Stephen George <sgit-turtle.testudo12@wronghead.com>
2011-12-08 19:12:31 -0600

    Fix: allowing deprecated use of :hoptoad_api_key in cap deploys

--- lib/toadhopper/capistrano.rb
+++ lib/toadhopper/capistrano.rb
@@ -9,7 +9,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     desc 'Notify Airbrake of the deployment'
     task :notify_airbrake, :except => {:no_release => true} do
       framework_env = fetch(:rails_env, fetch(:airbrake_env, 'production'))
-      api_key = fetch(:airbrake_api_key) || fetch(:hoptoad_api_key)
+      api_key = fetch(:hoptoad_api_key, nil) || fetch(:airbrake_api_key)
       puts 'Notifying Airbrake of deploy'
       options = {:framework_env => framework_env, :scm_revision => current_revision, :scm_repository => repository}
       Toadhopper(api_key).deploy!(options)
